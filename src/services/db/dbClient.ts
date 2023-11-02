0f701a8037f6238007ddfb16029d2598139e1171
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2023-11-02 14:14:34 +0000

    removed unneccesary var assignment

--- src/services/db/dbClient.ts
+++ src/services/db/dbClient.ts
@@ -1,16 +1,13 @@
 import { Sequelize } from 'sequelize'
 import { getEnv } from '../../utils/getEnv'
 
-const [dbHost, dbUser, dbPassword, dbName, dbPort] = [
-  getEnv('POSTGRES_HOST'),
+export const dbClient = new Sequelize(
+  getEnv('POSTGRES_DB'),
   getEnv('POSTGRES_USER'),
   getEnv('POSTGRES_PASSWORD'),
-  getEnv('POSTGRES_DB'),
-  Number(getEnv('POSTGRES_PORT'))
-]
-
-export const dbClient = new Sequelize(dbName, dbUser, dbPassword, {
-  host: dbHost,
-  port: dbPort,
-  dialect: 'postgres'
-})
+  {
+    host: getEnv('POSTGRES_HOST'),
+    port: Number(getEnv('POSTGRES_PORT')),
+    dialect: 'postgres'
+  }
+)
