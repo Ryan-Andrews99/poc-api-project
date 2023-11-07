893988e75eef990f8f6b02636a153ce4fd7e4d8d
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2023-11-07 14:57:04 +0000

    Adds log line to db connection

--- src/services/db/isDbActive.ts
+++ src/services/db/isDbActive.ts
@@ -3,6 +3,7 @@ import { dbClient } from './dbClient'
 export const isDbActive = async (): Promise<boolean> => {
   try {
     await dbClient.authenticate()
+    console.info('Connection has been established successfully.')
     return true
   } catch (error) {
     console.error('Unable to connect to the database:', error)
