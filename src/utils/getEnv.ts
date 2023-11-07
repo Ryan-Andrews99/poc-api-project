b9cdf46d716cb09fc7436940e40ae2efd4b6e62d
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2023-11-07 18:05:44 +0000

    moves to !env check instead of null || undefined

--- src/utils/getEnv.ts
+++ src/utils/getEnv.ts
@@ -1,11 +1,9 @@
 import { EnvironmentVariable } from './types/EnvironmentVariable'
-import 'dotenv/config'
 
 export const getEnv = (name: EnvironmentVariable) => {
   const env = process.env[name]
 
-  if (env === undefined || env === null)
-    throw Error(`Missing environment variable: ${name}`)
+  if (!env) throw Error(`Missing environment variable: ${name}`)
 
   return env
 }
